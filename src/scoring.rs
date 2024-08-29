28b3c7d15ed6490fdc0931f6ce04e1209bff704c
Tyler Dickson <tylercdickson@gmail.com>
2024-08-29 14:24:34 -0600

    Add tests to ensure ordering is correct for `GameStatus`

--- src/scoring.rs
+++ src/scoring.rs
@@ -133,15 +133,146 @@ impl std::fmt::Display for MoveScoreTurns {
 }
 
 impl MoveScoreTurns {
+    /// The absolute maximum a `MoveScoreTurns` instace can have
     pub const MAX: Self = Self {
         score: GameStatus::XWin,
         player_move: Point { x: 0, y: 0 },
-        turns_to_win: 0,
+        blank_squares_remaining: Board::EMPTY_BOARD_BLANK_SQUARES_REMAINING,
     };
 
+    /// The absolute minimum a `MoveScoreTurns` instace can have
     pub const MIN: Self = Self {
         score: GameStatus::OWin,
         player_move: Point { x: 0, y: 0 },
-        turns_to_win: 0,
+        blank_squares_remaining: Board::EMPTY_BOARD_BLANK_SQUARES_REMAINING,
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn xwin_is_greater_than_owin() {
+        let xwin = MoveScoreTurns {
+            score: GameStatus::XWin,
+            ..Default::default()
+        };
+
+        let owin = MoveScoreTurns {
+            score: GameStatus::OWin,
+            ..Default::default()
+        };
+
+        assert!(xwin > owin);
+    }
+
+    #[test]
+    fn xwin_is_greater_than_draw() {
+        let xwin = MoveScoreTurns {
+            score: GameStatus::XWin,
+            ..Default::default()
+        };
+
+        let draw = MoveScoreTurns {
+            score: GameStatus::Draw,
+            ..Default::default()
+        };
+
+        assert!(xwin > draw);
+    }
+
+    #[test]
+    fn xwin_is_greater_than_stillplaying() {
+        let xwin = MoveScoreTurns {
+            score: GameStatus::XWin,
+            ..Default::default()
+        };
+
+        let stillplaying = MoveScoreTurns {
+            score: GameStatus::StillPlaying,
+            ..Default::default()
+        };
+
+        assert!(xwin > stillplaying);
+    }
+
+    #[test]
+    fn owin_is_less_than_xwin() {
+        let xwin = MoveScoreTurns {
+            score: GameStatus::XWin,
+            ..Default::default()
+        };
+
+        let owin = MoveScoreTurns {
+            score: GameStatus::OWin,
+            ..Default::default()
+        };
+
+        assert!(owin < xwin);
+    }
+
+    #[test]
+    fn owin_is_less_than_draw() {
+        let owin = MoveScoreTurns {
+            score: GameStatus::OWin,
+            ..Default::default()
+        };
+
+        let draw = MoveScoreTurns {
+            score: GameStatus::Draw,
+            ..Default::default()
+        };
+
+        assert!(owin < draw);
+    }
+
+    #[test]
+    fn owin_is_less_than_stillplaying() {
+        let owin = MoveScoreTurns {
+            score: GameStatus::OWin,
+            ..Default::default()
+        };
+
+        let stillplaying = MoveScoreTurns {
+            score: GameStatus::StillPlaying,
+            ..Default::default()
+        };
+
+        assert!(owin < stillplaying);
+    }
+
+    #[test]
+    fn owin_more_blank_squares_is_less() {
+        let owin_more_blank_squares = MoveScoreTurns {
+            score: GameStatus::OWin,
+            blank_squares_remaining: 9,
+            ..Default::default()
+        };
+
+        let owin_less_blank_squares = MoveScoreTurns {
+            score: GameStatus::OWin,
+            blank_squares_remaining: 0,
+            ..Default::default()
+        };
+
+        assert!(owin_more_blank_squares < owin_less_blank_squares);
+    }
+
+    #[test]
+    fn xwin_more_blank_squares_is_greater() {
+        let xwin_more_blank_squares = MoveScoreTurns {
+            score: GameStatus::XWin,
+            blank_squares_remaining: 9,
+            ..Default::default()
+        };
+
+        let xwin_less_blank_squares = MoveScoreTurns {
+            score: GameStatus::OWin,
+            blank_squares_remaining: 0,
+            ..Default::default()
+        };
+
+        assert!(xwin_more_blank_squares > xwin_less_blank_squares);
+    }
 }
